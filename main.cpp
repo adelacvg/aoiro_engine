cd745a148801ec3484c3af938dc0b6b002c94c2d
adelacvg <adelacvgaoiro@163.com>
2022-09-22 22:23:15 +0800

    update

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 #include"src/graphics/buffers/indexbuffer.h"
 #include"src/graphics/buffers/vertexarray.h"
 
+#include"src/graphics/renderer2d.h"
+#include"src/graphics/renderable2d.h"
+#include"src/graphics/simple2drenderer.h"
+
 int main()
 {
     using namespace aoiro;
@@ -18,70 +22,18 @@ int main()
     Window window("Aoiro", 960, 540);
     // glClearColor(0,1,1,1);
 
-    // // std::cout<<glGetString(GL_VERSION)<<std::endl;
-    // GLuint vao;
-    // glGenVertexArrays(1, &vao);
-    // glBindVertexArray(vao);
-#if 0
-    GLfloat vertices[]{
-        0,0,0,
-        8,0,0,
-        0,3,0,
-        0,3,0,
-        8,3,0,
-        8,0,0
-    };
-    
-    GLuint vbo;
-    glGenBuffers(1,&vbo);
-    glBindBuffer(GL_ARRAY_BUFFER,vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,0);
-    glEnableVertexAttribArray(0);
-#else
-    GLfloat vertices[] = 
-    {
-        0,0,0,
-        0,3,0,
-        8,3,0,
-        8,0,0
-    };
-    GLushort indices[]=
-    {
-        0,1,2,
-        2,3,0
-    };
-    GLfloat colorsA[]=
-    {
-        1,0,1,1,
-        1,0,1,1,
-        1,0,1,1,
-        1,0,1,1
-    };
-    GLfloat colorsB[]=
-    {
-        0.2f,0.3f,0.8f,1,
-        0.2f,0.3f,0.8f,1,
-        0.2f,0.3f,0.8f,1,
-        0.2f,0.3f,0.8f,1
-    };
-    VertexArray sprite1,sprite2;
-    // Buffer* vbo=new Buffer(vertices, 4*3,3);
-    IndexBuffer ibo(indices,6);
 
-    sprite1.addBuffer(new Buffer(vertices, 4*3,3),0);
-    sprite1.addBuffer(new Buffer(colorsA, 4*4,4),1);
-    sprite2.addBuffer(new Buffer(vertices, 4*3,3),0);
-    sprite2.addBuffer(new Buffer(colorsB, 4*4,4),1);
-#endif
     mat4 ortho = mat4::orthographic(0,16,0,9,-1,1);
-
+    
 
     Shader shader("src/shaders/basic.vert",
                   "src/shaders/basic.frag");
     shader.enable();
     shader.setUniformMat4("pr_matrix",ortho);
     shader.setUniformMat4("ml_matrix", mat4::translation({4,3,0}));
+    Renderable2D sprite(maths::vec3(5,5,0),maths::vec2(4,4),maths::vec4(1,0,1,1),shader);
+    Renderable2D sprite1(maths::vec3(7,1,0),maths::vec2(2,3),maths::vec4(0.2,0,1,1),shader);
+    Simple2DRenderer renderer;
 
     shader.setUniform2f("light_pos",{4,1.5});
     shader.setUniform4f("colour", {0.2,0.3,0.8,1.0});
@@ -92,23 +44,10 @@ int main()
         double x,y;
         window.getMousePosition(x,y);
         shader.setUniform2f("light_pos",{float(x*16.0f/960.0f),float(9.0f-y*9.0f/540.0f)});
-#if 0
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-#else
-        sprite1.bind();
-        ibo.bind();
-        shader.setUniformMat4("ml_matrix", mat4::translation({4,3,0}));
-        glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT,0);
-        ibo.unbind();
-        sprite1.unbind();
+        renderer.submit(&sprite);
+        renderer.submit(&sprite1);
+        renderer.flush();
 
-        sprite2.bind();
-        ibo.bind();
-        shader.setUniformMat4("ml_matrix", mat4::translation({0,0,0}));
-        glDrawElements(GL_TRIANGLES, ibo.getCount(), GL_UNSIGNED_SHORT,0);
-        ibo.unbind();
-        sprite2.unbind();
-#endif
         window.update();
     }
     return 0;
